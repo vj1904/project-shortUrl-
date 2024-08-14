50184ccaf64367ae9d7063989a914de8fcec850c
@Venom1904 <vaibhavjaiswalvj19@gmail.com>
2024-08-14 15:12:00 +0530

    used jwt tokens

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,24 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secretKey = "Vaibhav@123$";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secretKey,
+    { expiresIn: "300s" }
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secretKey);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
